472ecf3d392d976abfd2a7d574fc44eed5aacba6
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-09-08 23:33:04 +0300

    1

--- app.py
+++ app.py
@@ -1,29 +1,67 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import urllib.parse
 import pathlib
+import mimetypes
+
+import json
+
 BASE_DIR = pathlib.Path()
 
 
-class MyHttpHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        print(urllib.parse.urlparse(self.path))
-        pr_url = urllib.parse.urlparse(self.path)
-        if pr_url.path == '/':
-            self.send_html_file('index.html')
-        elif pr_url.path == '/contact':
-            self.send_html_file('contact.html')
-        else:
-            self.send_html_file('error.html', 404)
+class HTTPHandler(BaseHTTPRequestHandler):
 
-    def send_html_file(self, filename, status=200):
-        self.send_response(status)
-        self.send_header('Content-type', 'text/html')
+    def do_POST(self):
+        # self.send_html_file('message.html')
+        body = self.rfile.read(int(self.headers['Content-Length']))
+        body = urllib.parse.unquote_plus(body.decode())
+        payload = {key: value for key, value in [
+            el.split('=') for el in body.split('&')]}
+        with open(BASE_DIR.joinpath('storage/data.json'), 'w', encoding='utf-8') as fd:
+            json.dump(payload, fd, ensure_ascii=False)
+        print(payload)
+
+        self.send_response(302)
+        self.send_header('Location', 'index.html')
+        self.end_headers()
+
+    def do_GET(self):
+        route = urllib.parse.urlparse(self.path)
+        match route.path:
+            case'/':
+                self.send_html_file('index.html')
+            case'#message':
+                self.send_html_file('message.html')
+            case'#blog':
+                self.send_html_file('blog.html')
+            case _:
+                file = BASE_DIR / route.path[1:]
+                if file.exists():
+                    self.send_static(file)
+
+                else:
+                    self.send_html_file('error.html', 404)
+
+    def send_html_file(self, filename, status_code=200):
+        self.send_response(status_code)
+        mime_type = mimetypes.guess_type(filename)
+        self.send_header('Content-type', mime_type)
+        self.end_headers()
+        with open(filename, 'rb') as fd:
+            self.wfile.write(fd.read())
+
+    def send_static(self, filename):
+        self.send_response(200)
+        mime_type, *rest = mimetypes.guess_type(filename)
+        if mime_type:
+            self.send_header('Content-type', mime_type)
+        else:
+            self.send_header('Content-type', 'text/plain')
         self.end_headers()
         with open(filename, 'rb') as fd:
             self.wfile.write(fd.read())
 
 
-def run(server_class=HTTPServer, handler_class=MyHttpHandler):
+def run(server_class=HTTPServer, handler_class=HTTPHandler):
     server_address = ('', 3000)
     http_server = server_class(server_address, handler_class)
     try:
