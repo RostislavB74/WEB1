508c1477c45de55d73edcf1da748075d203f247f
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-09-10 21:27:27 +0300

    edit

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 import json
+import logging
 import urllib.parse
 import pathlib
+import socket
+from threading import Thread
 import mimetypes
 from datetime import datetime
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-from jinja2 import Environment, FileSystemLoader
+# from jinja2 import Environment, FileSystemLoader
 
 BASE_DIR = pathlib.Path()
+SERVER_IP = '127.0.0.1'
+SERVER_PORT = 5000
+BUFFER = 1024
+
 # env - Environment(loader=-FileSystemLoader('templates'))
+
+
+def send_data_to_socket(body):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_socket.sendto(body, (SERVER_IP, SERVER_PORT))
 
 
 class HTTPHandler(BaseHTTPRequestHandler):
@@ -16,17 +28,7 @@ class HTTPHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         # self.send_html_file('message.html')
         body = self.rfile.read(int(self.headers['Content-Length']))
-        body = urllib.parse.unquote_plus(body.decode())
-        payload = {key: value for key, value in [
-            el.split('=') for el in body.split('&')]}
-        dt_msg = datetime.now()
-        dt_payload = {str(dt_msg): payload}
-        with open(BASE_DIR.joinpath('storage/data.json'), 'a', encoding='utf-8') as fd:
-
-            json.dump(dt_payload, fd, ensure_ascii=False)
-            fd.write(',\n')
-        # print(dt_payload)
-
+        send_data_to_socket(body)
         self.send_response(302)
         self.send_header('Location', 'index.html')
         self.end_headers()
@@ -36,7 +38,7 @@ class HTTPHandler(BaseHTTPRequestHandler):
         match route.path:
             case'/':
                 self.send_html_file('index.html')
-            case'#message':
+            case'/message':
                 self.send_html_file('message.html')
             case _:
                 file = BASE_DIR / route.path[1:]
@@ -75,5 +77,42 @@ def run(server_class=HTTPServer, handler_class=HTTPHandler):
         http_server.server_close()
 
 
+def save_data(data):
+    body = urllib.parse.unquote_plus(body.decode())
+    try:
+        payload = {key: value for key, value in [
+            el.split('=') for el in body.split('&')]}
+        dt_msg = datetime.now()
+        db_payload = {str(dt_msg): payload}
+        with open(BASE_DIR.joinpath('storage/data.json'), 'a', encoding='utf-8') as fd:
+            json.dump(db_payload, fd, ensure_ascii=False, indent=5)
+            fd.write(",\n")
+    except ValueError as err:
+        logging.error(f"Field parse data:{body} with error {err}")
+    except OSError as err:
+        logging.error(f"Field parse data:{body} with error {err}")
+
+
+def run_socket_server(ip, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server = ip, port
+    server_socket.bind((server))
+
+    try:
+        while True:
+            data, address = server_socket.recvfrom(BUFFER)
+            save_data(data)
+    except KeyboardInterrupt:
+        logging.info('Socket server stopped')
+    finally:
+        server_socket.close()
+
+
 if __name__ == '__main__':
-    run()
+    logging.basicConfig(level=logging.INFO,
+                        format="%(threadName)s %(message)s")
+
+    thread_server = Thread(target=run)
+    thread_server.start()
+    thread_socket = Thread(target=run_socket_server(SERVER_IP, SERVER_PORT))
+    thread_socket.start()
